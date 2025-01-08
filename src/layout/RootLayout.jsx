009f52da9710f90fcdece82d5f832ec4d6c20c60
Mimoun <156517583+1Dh2Be@users.noreply.github.com>
2025-01-08 20:21:04 +0100

    Successfully added page transition

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,13 +1,64 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Container from "../components/Container";
 import Nav from "../components/Nav";
+import { AnimatePresence, motion } from "framer-motion";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const anim = (variants, custom) => {
+    return {
+      initial: "initial",
+      animate: "enter",
+      exit: "exit",
+      variants,
+      custom,
+    };
+  };
+
+  const transitionPage = {
+    initial: { top: 0 },
+    enter: (i) => ({
+      top: "100%",
+      transition: {
+        duration: 0.5,
+        delay: 0.1 * i,
+      },
+      transitionEnd: {
+        height: 0,
+        top: 0,
+      },
+    }),
+    exit: (i) => ({
+      height: "100%",
+      transition: {
+        duration: 0.5,
+        delay: 0.1 * i,
+      },
+    }),
+  };
+
+  const nbOfCulumns = 5;
+
   return (
     <Container>
       <Nav />
-      <main className="flex justify-center items-center h-full text-6xl">
-        <Outlet />
+      <main className="flex justify-center items-center h-[90%] text-6xl">
+        <AnimatePresence mode="wait">
+          <motion.div key={location.pathname}>
+            <div className="h-screen w-screen fixed top-0 left-0 pointer-events-none flex">
+              {[...Array(nbOfCulumns)].map((_, i) => {
+                return (
+                  <motion.div
+                    {...anim(transitionPage, nbOfCulumns - i)}
+                    key={i}
+                    className="h-full w-full relative bg-zinc-950"
+                  />
+                );
+              })}
+            </div>
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
     </Container>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,5 @@
-import { transition } from "../components/transition";
-
 const About = () => {
   return <div>About</div>;
 };
 
-export default transition(About);
+export default About;
